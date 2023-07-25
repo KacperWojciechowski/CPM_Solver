e4fac0e6789fae25f2f3ee3f9ac43cc17ed5c9cc
Kacper Wojciechowski <k.woj.coding@gmail.com>
2023-07-25 22:02:44 +0200

    refactored cmake

--- inc/serializer/XmlSerializer.hpp
+++ inc/serializer/XmlSerializer.hpp
@@ -3,8 +3,13 @@
 #include <data/JobList.hpp>
 
 namespace cpm::serializers {
+
+template <typename TargetData>
 class XmlSerializer 
 {
-
+    auto deserialize(std::string sourceFileName) -> TargetData
+    {
+        
+    }
 };
 } // namespace cpm::serializers
---
+++ src/ut/empty.cpp
@@ -0,0 +1,1 @@
+// placeholder for creating the executable in cmake

d5f52f8b5a0eb2068cc2bf010ff98305412708f3
Kacper Wojciechowski <k.woj.coding@gmail.com>
2023-08-26 19:02:26 +0200

    saved AttributeTree

--- src/data/AttributeTree.cpp
+++ src/data/AttributeTree.cpp
@@ -1,18 +1,12 @@
 #include <data/AttributeTree.hpp>
 
+#include <ranges>
+
 namespace {
 using NodeSet = std::set<cpm::data::AttributeTree::Node>;
 
-auto contains(const NodeSet& nodeSet, std::string_view nodeName) -> bool
-{
-    return false;
-}
 } // namespace
 
 namespace cpm::data {
-auto AttributeTree::contains(std::string_view nodeName) const noexcept -> bool
-{
-    if (root.name == nodeName) return true;
-    return ::contains(root.children, nodeName);
-}
+
 } // namespace cpm::data
